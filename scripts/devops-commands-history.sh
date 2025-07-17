c1fb157d24dbdc5703eab3df40496e304b446ea0
tybali <tayyabalihere@gmail.com>
2025-07-17 08:19:40 +0500

    setting up pipeline

---
+++ scripts/devops-commands-history.sh
@@ -0,0 +1,3 @@
+sudo chown -R administrator:www-data /var/www/manahilejaz.pro/
+administrator@ds-core:/var/www/manahilejaz.pro$ sudo chmod -R 775 /var/www/manahilejaz.pro/
+administrator@ds-core:/var/www/manahilejaz.pro$ pnpm run build
